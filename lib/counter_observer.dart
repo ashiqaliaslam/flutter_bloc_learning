13daab94e26b8c6f9ebc2b442c737540b3162632
Ashique Ali <60208343+ashiqaliaslam@users.noreply.github.com>
2023-12-02 11:06:24 +0500

    counter app built with bloc cubit

---
+++ lib/counter_observer.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: avoid_print
+
+import 'package:bloc/bloc.dart';
+
+/// observes all state changes
+class CounterObserver extends BlocObserver {
+  const CounterObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+}
