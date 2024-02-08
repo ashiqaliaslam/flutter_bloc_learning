69001f75f8188f5b88504b21b15221c082638aa0
Ashique Ali <60208343+ashiqaliaslam@users.noreply.github.com>
2024-02-08 09:49:29 +0500

    login app complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-import 'package:bloc/bloc.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_learning/app.dart';
-import 'package:flutter_bloc_learning/simple_bloc_observer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login/app.dart';
 
-void main() {
-  Bloc.observer = const SimpleBlocObserver();
-  runApp(const App());
-}
+void main() => runApp(const App());
