741ccad8a6e71a56e231e06c3bbe12c6650e4864
Ashique Ali <60208343+ashiqaliaslam@users.noreply.github.com>
2024-02-17 22:04:51 +0500

    weather app

--- lib/app.dart
+++ lib/app.dart
@@ -1,89 +1,47 @@
-import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_login/authentication/bloc/authentication_bloc.dart';
-import 'package:flutter_login/home/view/home_page.dart';
-import 'package:flutter_login/login/view/login_page.dart';
-import 'package:flutter_login/splash/view/splash_page.dart';
-import 'package:user_repository/user_repository.dart';
+import 'package:flutter_weather/theme/theme.dart';
+import 'package:flutter_weather/weather/weather.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:weather_repository/weather_repository.dart';
 
-class App extends StatefulWidget {
-  const App({super.key});
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({required WeatherRepository weatherRepository, super.key})
+      : _weatherRepository = weatherRepository;
 
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  late final AuthenticationRepository _authenticationRepository;
-  late final UserRepository _userRepository;
-
-  @override
-  void initState() {
-    super.initState();
-    _authenticationRepository = AuthenticationRepository();
-    _userRepository = UserRepository();
-  }
-
-  @override
-  void dispose() {
-    _authenticationRepository.dispose();
-    super.dispose();
-  }
+  final WeatherRepository _weatherRepository;
 
   @override
   Widget build(BuildContext context) {
     return RepositoryProvider.value(
-      value: _authenticationRepository,
+      value: _weatherRepository,
       child: BlocProvider(
-        create: (_) => AuthenticationBloc(
-          authenticationRepository: _authenticationRepository,
-          userRepository: _userRepository,
-        ),
-        child: const AppView(),
+        create: (_) => ThemeCubit(),
+        child: const WeatherAppView(),
       ),
     );
   }
 }
 
-class AppView extends StatefulWidget {
-  const AppView({super.key});
-
-  @override
-  State<AppView> createState() => _AppViewState();
-}
-
-class _AppViewState extends State<AppView> {
-  final _navigatorKey = GlobalKey<NavigatorState>();
-
-  NavigatorState get _navigator => _navigatorKey.currentState!;
+class WeatherAppView extends StatelessWidget {
+  const WeatherAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: _navigatorKey,
-      builder: (context, child) {
-        return BlocListener<AuthenticationBloc, AuthenticationState>(
-          listener: (context, state) {
-            switch (state.status) {
-              case AuthenticationStatus.authenticated:
-                _navigator.pushAndRemoveUntil<void>(
-                  HomePage.route(),
-                  (route) => false,
-                );
-              case AuthenticationStatus.unauthenticated:
-                _navigator.pushAndRemoveUntil<void>(
-                  LoginPage.route(),
-                  (route) => false,
-                );
-              case AuthenticationStatus.unknown:
-                break;
-            }
-          },
-          child: child,
+    return BlocBuilder<ThemeCubit, Color>(
+      builder: (context, color) {
+        return MaterialApp(
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
+            colorScheme: ColorScheme.fromSeed(seedColor: color),
+            textTheme: GoogleFonts.rajdhaniTextTheme(),
+          ),
+          home: const WeatherPage(),
         );
       },
-      onGenerateRoute: (_) => SplashPage.route(),
     );
   }
 }
---
+++ packages/weather_repository/test/src/models/weather_test.dart
@@ -0,0 +1,15 @@
+// import 'package:test/test.dart';
+// import 'package:weather_repository/weather_repository.dart';
+
+// void main() {
+//   group('Weather', () {
+//     test('can be (de)serialized', () {
+//       final weather = Weather(
+//         location: WeatherCondition.cloudy,
+//         temperature: 10.2,
+//         condition: 'Chicago',
+//       );
+//       expect(Weather.fromJson(weather.toJson()), equals(weather));
+//     });
+//   });
+// }
